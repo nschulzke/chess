893ec063bac35debf8487327af64c45e4add67fa
Nathan Schulzke <nschulzke@gmail.com>
2018-02-26 17:41:50 -0700

    Initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="board" id="board">
+  <div class="row" v-for="(a, i) in 8">
+    <div class="space" v-bind:id="spaceId(i, j)" v-for="(b, j) in 8">
+      <span class="piece" v-bind:class="getPiece(i, j)"></span>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+const COLS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+import Board from "@/lib/board.js"
+export default {
+  name: 'Board',
+  data() {
+    return {
+      board: new Board()
+    }
+  },
+  methods: {
+    spaceId: function(row, col) {
+      col = COLS[col]
+      row = row + 1;
+      return col + row;
+    },
+    coords: function(string) {
+      col = COLS.findIndex((item) => item === string[0]);
+      row = Number(string[1]) - 1;
+      return { col: col, row: row };
+    },
+    getPiece: function(row, col) {
+      if (this.board.getPiece(row, col) !== undefined)
+        return this.board.getPiece(row, col).class();
+      else return '';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/lib/board.js
@@ -0,0 +1,81 @@
+class Piece {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  white() {
+    this.color = 'white';
+    return this;
+  }
+
+  black() {
+    this.color = 'black';
+    return this;
+  }
+
+  class() {
+    return this.name + ' ' + this.color;
+  }
+}
+
+const PIECES = {
+  pawn: function() {
+    return new Piece('pawn', 1);
+  },
+  knight: function() {
+    return new Piece('knight', 3);
+  },
+  bishop: function() {
+    return new Piece('bishop', 3);
+  },
+  rook: function() {
+    return new Piece('rook', 5);
+  },
+  queen: function() {
+    return new Piece('queen', 9);
+  },
+  king: function() {
+    return new Piece('king', -1);
+  },
+};
+
+const DEFAULT_SETUP = [
+  [PIECES.pawn, PIECES.pawn,   PIECES.pawn,   PIECES.pawn,  PIECES.pawn, PIECES.pawn,   PIECES.pawn,   PIECES.pawn],
+  [PIECES.rook, PIECES.knight, PIECES.bishop, PIECES.queen, PIECES.king, PIECES.bishop, PIECES.knight, PIECES.rook]
+]
+
+var DEFAULT_BOARD = [];
+for (var i = 0; i < 8; i++) {
+  DEFAULT_BOARD[i] = [];
+}
+for (var i = 0; i < 8; i++) {
+  DEFAULT_BOARD[0][i] = DEFAULT_SETUP[1][i]().black();
+  DEFAULT_BOARD[1][i] = DEFAULT_SETUP[0][i]().black();
+  DEFAULT_BOARD[6][i] = DEFAULT_SETUP[0][i]().white();
+  DEFAULT_BOARD[7][i] = DEFAULT_SETUP[1][i]().white();
+}
+Object.freeze(DEFAULT_BOARD);
+
+export default class Board {
+  constructor() {
+    this.board = [];
+    for (var i = 0; i < 8; i++) {
+      this.board.push([]);
+      for (var j = 0; j < 8; j++) {
+        this.board[i].push(DEFAULT_BOARD[i][j]);
+      }
+    }
+  }
+
+  movePiece(start, dest) {
+    if (this.board[dest.row][dest.col] === undefined) {
+      this.board[dest.row].splice(dest.col, 1, this.board[start.row][start.col]);
+      this.board[start.row].splice(start.col, 1, undefined);
+    }
+  }
+
+  getPiece(row, col) {
+    return this.board[row][col];
+  }
+}
