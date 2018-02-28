3cdf914368990553f2b7b6c94a0b705e158ca797
Nathan Schulzke <nschulzke@gmail.com>
2018-02-28 13:23:26 -0700

    Add home page

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <h1>Chess</h1>
+    <p>
+      This is a simple chess game in Vue. It's not quite complete, because the project
+      was bigger than I anticipated, but you can do most moves and play a simple game.
+
+      Things that I'm going to do later:
+    </p>
+    <ul>
+      <li>Check and checkmate detection</li>
+      <li>Castling</li>
+      <li>Timers</li>
+      <li>en Passant capturing</li>
+    </ul>
+    <a href="#/game">Go to the game!</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style>
+  .main {
+    width: 40rem;
+    margin: 0 auto;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
 import Game from '@/components/Game'
 
 Vue.use(Router);
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/game',
       name: 'Game',
       component: Game
     }
